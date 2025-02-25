ca84be12007df2b892a6502e5aac182befdd6e48
Markus Knapp <github@direcs.de>
2025-02-25 18:34:30 +0100

    added all-new pigpiod service

--- test/test.py
+++ test/test.py
@@ -15,13 +15,18 @@ pi = pigpio.pi()  # Verbindung zum pigpio-Daemon
 
 print("Connecting to Raspi...")
 if not pi.connected:
+    print("ERROR")
     exit()
 
 try:
     while True:
+        print("ON")
         pi.write(PIN, 1)  # HIGH setzen
         time.sleep(1)
+        print("OFF")
         pi.write(PIN, 0)  # LOW setzen
         time.sleep(1)
 except KeyboardInterrupt:
+    print("OFF")
+    pi.write(PIN, 0)  # LOW setzen
     pi.stop()  # Verbindung schließen
