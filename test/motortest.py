de9e239b25fb005fd1444a902b06c1dbeadb075c
Markus Knapp <github@direcs.de>
2025-02-25 22:34:59 +0100

    added PWM for slower movement

--- test/motortest.py
+++ test/motortest.py
@@ -9,6 +9,8 @@ import time
 
 MotorA1In3 = 4     # GPIO
 MotorA1In4 = 27    # GPIO
+MotorA1En  = 17    # GPIO (Enable / PWM)
+MotorA1PWM = 200   # PWM duty cycle (0-255) aka "motor max speed"
 
 
 pi = pigpio.pi()  # connect to pigpio daemon
@@ -21,18 +23,26 @@ try:
     while True:
         # Motor RIGHT
         print("Motors RIGHT")
+        pi.set_PWM_dutycycle(MotorA1En, MotorA1PWM)
         pi.write(MotorA1In3, 1)
         pi.write(MotorA1In4, 0)
         time.sleep(1)
 
         # Motor LEFT
         print("Motors LEFT")
+        pi.set_PWM_dutycycle(MotorA1En, MotorA1PWM)
         pi.write(MotorA1In3, 0)
         pi.write(MotorA1In4, 1)
+        time.sleep(1)
+
+        # Motor OFF
+        print("Motors OFF")
+        pi.set_PWM_dutycycle(MotorA1En, 0)
         time.sleep(1)
 except KeyboardInterrupt:
     # Motor OFF
     print("Motors OFF")
+    pi.set_PWM_dutycycle(MotorA1En, 0)
     pi.write(MotorA1In3, 0)
     pi.write(MotorA1In4, 0)
     pi.stop()  # close connection
