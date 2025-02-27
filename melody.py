58ea7881377adba908dd19f93b80e98023aa51c2
Markus Knapp <github@direcs.de>
2025-02-27 23:14:49 +0100

    migrated to pigpio

--- melody.py
+++ melody.py
@@ -1,36 +1,42 @@
 #!/usr/bin/python
 # coding=utf-8
 
-import RPi.GPIO as GPIO
+import pigpio
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
 BUZZER = 13
-GPIO.setup(BUZZER, GPIO.OUT)
+
+pi = pigpio.pi()  # connecting to pigpio daemon
+print("Connecting to Raspi...")
+if not pi.connected:
+    exit()
+
+pi.set_mode(BUZZER, pigpio.OUTPUT)
 
 def buzz(noteFreq, duration):
-    halveWaveTime = 1 / (noteFreq * 2 )
+    halveWaveTime = 1 / (noteFreq * 2)
     waves = int(duration * noteFreq)
     for i in range(waves):
-       GPIO.output(BUZZER, True)
-       time.sleep(halveWaveTime)
-       GPIO.output(BUZZER, False)
-       time.sleep(halveWaveTime)
+        pi.write(BUZZER, 1)
+        time.sleep(halveWaveTime)
+        pi.write(BUZZER, 0)
+        time.sleep(halveWaveTime)
 
 def play():
-    t=0
-    dur=0.09
+    t = 0
+    dur = 0.09
     # the notes have these frequencies: https://pages.mtu.edu/~suits/notefreqs.html
     # Bruder Jakob
     # notes=[262,294,330,262,262,294,330,262,330,349,392,330,349,392,392,440,392,349,330,262,392,440,392,349,330,262,262,196,262,262,196,262]
     # duration=[0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,1,0.5,0.5,1,0.25,0.25,0.25,0.25,0.5,0.5,0.25,0.25,0.25,0.25,0.5,0.5,0.5,0.5,1,0.5,0.5,1]
-    notes    = [392,   392, 392,   1,  294,  392,  523,    1, 262]
-    duration = [dur,   dur, dur, 0.3,  dur,  dur,  dur,  0.5, dur]
+    notes = [392, 392, 392, 1, 294, 392, 523, 1, 262]
+    duration = [dur, dur, dur, 0.3, dur, dur, dur, 0.5, dur]
     for n in notes:
         buzz(n, duration[t])
         # pause between each note
-        time.sleep(duration[t] *0.4)
-        t+=1
+        time.sleep(duration[t] * 0.4)
+        t += 1
 
 play()
+
+pi.stop()  # close connection
