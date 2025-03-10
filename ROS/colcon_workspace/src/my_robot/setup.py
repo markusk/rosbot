ab53a9484d23a4a59bccb61da34f4f6f4acaba50
Markus Knapp <github@direcs.de>
2025-03-10 22:56:12 +0100

    added missing setup.py

--- ROS/colcon_workspace/src/my_robot/setup.py
+++ ROS/colcon_workspace/src/my_robot/setup.py
@@ -1,26 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'my_robot'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    version='0.0.1',
+    packages=[package_name],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='markus',
-    maintainer_email='github@direcs.de',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    maintainer='Markus',
+    maintainer_email='markus@example.com',
+    description='Mein ROS 2 Paket',
+    license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-	        'my_node = my_robot.scripts.my_node:main',
+            'my_node = my_robot.scripts.my_node:main',
         ],
     },
+    data_files=[
+        (os.path.join('share', package_name), ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
 )
